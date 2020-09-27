51891710274a8048fe6f247aa6d0bc0d5acc9a9f
jonathanLozaiza <jlrcontactos@gmail.com>
2020-09-27 10:56:47 -0500

    create server apollo

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const {ApolloServer, gql} = require("apollo-server")
 //Servidor
 const server = new ApolloServer()
 
+//Schema
+
+const typeDefs = gql`
+
+    type Curso {
+        nombre:String
+        tecnologia:String
+    }
+
+    type Query {
+        obtenerCursos:Curso
+    }
+`
+
+//Resolvers
+
+const resolvers = {
+    Query:{
+        obtenerCursos
+    }
+}
+
+
 //Arrancar el servidor
 server.listen().then(({url})=>{
     console.log(`el servidor esta corriendo en la url ${url}`)
